4445ed8160e46ba5241883cab01d474f0ff122be
ThanhDat-debug <az01667600591@gmail.com>
2025-04-06 03:37:21 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/YourUsername/user_auth
+
+go 1.24.1
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var users = make(map[string]string)
+var loggedInUser string
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/welcome", welcomeHandler)
+	http.HandleFunc("/logout", logoutHandler)
+	fmt.Println("Server is running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		if _, exists := users[username]; exists {
+			http.Error(w, "Username already taken", http.StatusConflict)
+			return
+		}
+		users[username] = password
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	tmpl, err := template.ParseFiles("templates/register.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		storedPassword, exists := users[username]
+		if !exists || storedPassword != password {
+			http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+			return
+		}
+
+		loggedInUser = username
+		http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/login.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	if loggedInUser == "" {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/welcome.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, loggedInUser)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	loggedInUser = ""
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
